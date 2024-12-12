e765399c7ee55a7be5ea3019909944a6c61f23ea
DrojasFrontend <daniel.rojas@leggercolombia.com>
2024-12-12 15:14:27 -0500

    upload img

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,14 @@ export default function Document() {
 					href="https://fonts.googleapis.com/css2?family=Anton&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
 					rel="stylesheet"
 				></link>
+
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="viveclaro.co" />
+				<meta property="og:title" content="Viveclaro" />
+				<meta property="og:description" content="Disfruta en familia y con amigos de espacios únicos, tanto cubiertos como al aire libre" />
+				<meta property="og:image" content="/public/images/logo-red.png" />
+				<meta property="og:locale" content="es_ES" />
+				<meta property="og:site_name" content="viveclaro" />
 			</Head>
 			<body>
 				<Main />
